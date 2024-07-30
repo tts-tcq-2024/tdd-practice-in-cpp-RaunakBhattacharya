da3b16be24b33d396d291e08a806fcfd7ae103a6
RaunakBhattacharya <160215825+RaunakBhattacharya@users.noreply.github.com>
2024-07-30 23:18:17 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -6,4 +6,6 @@ public:
     int Add(const std::string& numbers);
 private:
     std::vector<int> splitNumbers(const std::string& numbers, const std::string& delimiter);
+    std::string getDelimiterAndNumbersPart(const std::string& numbers, std::string& delimiter);
+    int calculateSum(const std::vector<int>& nums);
 };
